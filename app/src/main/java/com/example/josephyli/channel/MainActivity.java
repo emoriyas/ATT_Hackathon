74639d4374db6b97a76a07e6139299d0f3694fdf
Joseph Li <josephyli@gmail.com>
2017-03-10 20:51:42 -1000

    First commit

---
+++ app/src/main/java/com/example/josephyli/channel/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.josephyli.channel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
